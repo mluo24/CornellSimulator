d3ae0ec1b58d0eea8f2634533a52247f51fd2be5
Miranda Luo <mml267@cornell.edu>
2021-03-26 23:52:55 -0400

    added Author complication unit, install.txt

---
+++ author.mli
@@ -0,0 +1,16 @@
+(************************************************************ 
+   Copyright (C) 2021 Cornell University.
+   Created by Michael Clarkson (mrc26@cornell.edu) and CS 3110 course staff.
+   You may not redistribute this assignment, distribute any derivatives,
+   or use it for commercial purposes.
+ ************************************************************)
+
+(** CS 3110 Spring 2021 Final Project M1
+
+    @author Miranda Luo (mml267) 
+    @author Ka-Hyun Nam (kn294)
+    @author Mina Prapakamol (jp889)
+    *)
+
+(** [hours_worked] is the number of hours you worked on this assignment. *)
+val hours_worked : int
