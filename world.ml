9dff7270dff567eacf08fdc8c708d9500b904a6c
kn294 <kn294@cornell.edu>
2021-03-31 02:55:43 -0400

    added unimplemented world.ml

---
+++ world.ml
@@ -0,0 +1,21 @@
+type t
+
+type tile =
+  | Grass
+  | Building
+
+let x_dim = failwith "Unimplemented"
+
+let y_dim = failwith "Unimplemented"
+
+let tile_sizw = failwith "Unimplemented"
+
+let rows = failwith "Unimplemented"
+
+let cols = failwith "Unimplemented"
+
+let arr_from_txt = failwith "Unimplemented"
+
+let int_to_tile = failwith "Unimplemented"
+
+let map_from_arr = failwith "Unimplemented"
