fe27875835d8c673737b0daf7a41d707a649c061
kn294 <kn294@cornell.edu>
2021-04-01 16:03:16 -0400

    add key inputs

--- state.ml
+++ state.ml
@@ -9,21 +9,40 @@ type state = {
 (* { character: Character; world: time: } *)
 
 (* for now, the character is a black dot *)
+exception End
 
-let dot x y = 
+let key_input key init the_end =
+  init;
+  try
+    while true do
+      try
+        let s = Graphics.wait_next_event [ Graphics.Key_pressed ] in
+        if s.Graphics.keypressed then key s.Graphics.key
+        else print_string "Wrong key. You can only use char keys!"
+      with End -> the_end
+    done
+  with End -> the_end
+
+let dot s =
   Graphics.set_color black;
-  Graphics.fill_circle x y 20;
+  Graphics.fill_circle s.x s.y 20
 
-let dot_init x y  =
+let dot_init s =
   Graphics.open_graph "";
   set_window_title "Cornell Simulator";
-  resize_window 600 600;
-  dot x y;
+  resize_window 800 400;
+  dot s
 
 let dot_end =
   Graphics.close_graph ();
-  print_string "Good BYE"
+  print_string "End of dot."
 
-let move_key s c = 
-  match c with 
-  | '
+let move_key s c =
+  dot s;
+  match c with
+  | 'w' -> if s.y < 400 then s.y <- s.y + 1
+  | 'a' -> if s.x > 0 then s.x <- s.x - 1
+  | 's' -> if s.y > 0 then s.y <- s.y + 1
+  | 'd' -> if s.x < 800 then s.x <- s.x + 1
+  | 'e' -> dot_end
+  | _ -> ()
--- state.mli
+++ state.mli
@@ -4,9 +4,14 @@ type state
 
 (* { character: Character; world: time: } *)
 
-(** stand in character for now is dot and it's init pos is center *)
+val key_input : (char -> unit) -> 'a -> 'b -> unit
+
+(** stand in character for now is dot, and it's init position is 400 200 *)
+val dot : unit
+
 val dot_init : unit
 
 val dot_end : unit
 
 (** match keyboard input to a move *)
+val move_key : state -> char -> unit
