5337e700b8009f475db51adf33085dfc8f39edae
kn294 <kn294@cornell.edu>
2021-04-01 16:37:23 -0400

    keyboard inputs works, but not quite what it's supposed to be, it is stretchy dot

--- state.ml
+++ state.ml
@@ -43,6 +43,6 @@ let move_key s c =
   | 'w' -> if s.y < 400 then s.y <- s.y + 1
   | 'a' -> if s.x > 0 then s.x <- s.x - 1
   | 's' -> if s.y > 0 then s.y <- s.y + 1
-  | 'd' -> if s.x < 800 then s.x <- s.x + 1
+  | 'd' -> if s.x < 800 then s.x <- s.x - 1
   | 'e' -> dot_end
   | _ -> ()
