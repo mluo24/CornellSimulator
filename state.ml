53b1d837e610e995af1799a4cbfac6e493a8d507
kn294 <kn294@cornell.edu>
2021-04-01 12:03:42 -0400

    clean main

--- state.ml
+++ state.ml
@@ -6,5 +6,8 @@ type t
 (* { character: Character; world: time: } *)
 
 let draw_point =
+  Graphics.open_graph "";
+  (* set_window_title "Cornell Simulator"; *)
+  resize_window 600 600;
   Graphics.set_color black;
   Graphics.fill_circle 300 300 20
