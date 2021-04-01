d9d2319a202303073a2aa15dadaf13becad6bce8
Miranda Luo <mml267@cornell.edu>
2021-04-01 01:06:18 -0400

    implemented map sig and added testmap

--- main.ml
+++ main.ml
@@ -2,23 +2,25 @@ open Graphics
 open World
 open Unix
 
-(* infinite loop *)
+(** infinite loop *)
 let rec loop () = loop ()
 
-(* Opens the graph. If closed, catch fatal I/O error and exit*)
+(** Opens the graph. If closed with x button, catch fatal I/O error and exit *)
 let main () =
   try
     Graphics.open_graph "";
     set_window_title "Cornell Simulator";
     resize_window World.x_dim World.y_dim;
     loop ()
-  with Graphic_failure x ->
-    print_string ("Goodbye. " ^ x);
-    print_newline ()
+  with Graphic_failure x -> begin 
+    match x with 
+    | "fatal I/O error" -> print_endline "Goodbye.";
+    | _ -> print_endline (x ^ "Run export DISPLAY=:0 and make sure X server is running.");
+  end
 
 (* let main () = open_graph ""; set_window_title "Hello"; draw_string
    "WELCOME "; print_endline "Press enter to exit:"; let s = read_line
    () in if s = s then close_graph () *)
 
-(* runs the program*)
+(** runs the program *)
 let () = main ()
--- world.ml
+++ world.ml
@@ -1,30 +1,48 @@
+open Yojson.Basic.Util
 (* animation loading tilesets *)
 
-type tile =
-  | Grass
+type tile = 
+  | Blank
+  | Grass 
+  | Sidewalk
   | Building
 
-type t = tile array array
+type t = {
+  cols : int;
+  rows : int;
+  tile_size : int;
+  tiles: tile array
+}
 
 let x_dim = 800
 
 let y_dim = 640
 
-let tile_size = 16
+(* let tile_size = 16
 
 let rows = x_dim / tile_size
 
 let cols = y_dim / tile_size
 
-let layers = 2
+let layers = 2 *)
 
-let arr_from_txt filename = failwith "Unimplemented"
+let int_to_tile i = 
+  match i with
+  | 1 -> Grass
+  | 2 -> Sidewalk
+  | 3 -> Building
+  | _ -> Blank
 
+let map_from_json json = {
+  cols = json |> member "cols" |> to_int;
+  rows = json |> member "rows" |> to_int;
+  tile_size = json |> member "tile_size" |> to_int;
+  tiles = json |> member "tile_size" |> to_list 
+          |> List.map to_int |> List.map int_to_tile |> Array.of_list
+}
 (* let ic = open_in filename in let try_read () = try Some (input_line
    ic) with End_of_file -> None in let rec loop acc = match try_read ()
    with | Some s -> loop (s :: acc) | None -> close_in ic; List.rev acc
    in loop [] *)
 
 let map_from_arr arr = failwith "Unimplemented"
-
-let int_to_tile i = failwith "Unimplemented"
