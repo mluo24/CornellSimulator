24576c2193103f5e0ec7bc3346ac38a463d1c480
MinaP <jitrada.prapakamol@gmail.com>
2021-04-08 19:31:07 -0400

    add specification

--- character.mli
+++ character.mli
@@ -1,11 +1,15 @@
-(* player character *)
 open Graphics
 
+(** The abstract type of values representing character. *)
 type t
 
-(* current position*)
+(** [get_position t] is the position on graphical interface of character [t]*)
 val get_position : t -> Position.t
 
+(** [move t ch] moves character [t] to the new position in the direction
+    specified by character [ch] based on the character current speed Require:
+    ch must be 'w', 'a', 's', 'd' which represent up, left, down, right
+    respectively*)
 val move : t -> char -> unit
 
 (* for drawing player *)
@@ -13,6 +17,7 @@ val move : t -> char -> unit
 
 val get_user_name : t -> string
 
+(** [get_size t ] is the size of character [t] in graphical interface unit*)
 val get_size : t -> int
 
 (* val get_level: t -> how getting an item effect user *)
@@ -20,4 +25,6 @@ val get_size : t -> int
 
 val draw : t -> unit
 
+(** [init_character] initialize fake character TODO: re implement to
+    initialize the character from file*)
 val init_character : unit -> t
--- position.mli
+++ position.mli
@@ -1,3 +1,5 @@
+(** The abstract type of values representing the position on the graphical
+    interface. x = 0, y= is bottom left conner *)
 type t = {
   mutable x : int;
   mutable y : int;
@@ -5,6 +7,7 @@ type t = {
 
 val from_json : Yojson.Basic.t -> t
 
+(** [distance pt1 pt2] is distance between pt1 and pt2 *)
 val distance : t -> t -> int
 
 (* check valid *)
